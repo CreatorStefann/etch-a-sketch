3a86ce465f267ffbdaafe86fb4dbbbfaf63683c0
CreatorStefann <stefansandor26@gmail.com>
2023-07-29 19:33:59 +0300

    Added a button to set the grid size

--- scripts.js
+++ scripts.js
@@ -1,31 +1,58 @@
-function createGrid(){
-    const container = document.querySelector('#container');
-    for(let i=0; i<16; i++){
-        for(let j=0; j<16; j++){
-            const div = document.createElement('div');
-            div.classList.add('grid-item');
-            container.appendChild(div);
-        }
+let gridSize = 16;
+const container = document.querySelector('#container');
+const divs = [];
+
+function createGrid() {
+  const masura = 512 / gridSize;
+
+  // Clear previous grid items if any
+  divs.forEach((div) => div.remove());
+  divs.length = 0;
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid-item');
+    div.style.width = `${masura}px`;
+    div.style.height = `${masura}px`;
+    container.appendChild(div);
+    divs.push(div);
+  }
+}
+
+function hover() {
+  let isMouseDown = false;
+
+  container.addEventListener('mousedown', () => {
+    isMouseDown = true;
+  });
+
+  container.addEventListener('mouseup', () => {
+    isMouseDown = false;
+  });
+
+  divs.forEach((div) => {
+    div.addEventListener('mouseenter', () => {
+      if (isMouseDown) {
+        div.style.backgroundColor = 'black';
+      }
+    });
+  });
+}
+
+function size() {
+  let button = document.querySelector('button');
+  button.addEventListener('click', () => {
+    let gridSizeString = prompt("How many squares do you want to see on a side?");
+    const newGridSize = parseInt(gridSizeString);
+
+    if (newGridSize) {
+      gridSize = newGridSize;
+      createGrid();
+      hover();
     }
+  });
 }
 
-function hover(){
-    const divs = document.querySelectorAll('.grid-item')
-    let isMouseDown = false;
-    divs.forEach((div) => {
-        div.addEventListener('mousedown', () => {
-            isMouseDown = true;
-            div.style.backgroundColor = 'black';
-        });
-        div.addEventListener('mouseenter', () => {
-            if(isMouseDown)
-                div.style.backgroundColor = 'black';
-        })
-        div.addEventListener('mouseup', () => {
-            isMouseDown = false;
-        })
-    }) 
-}
-
+size();
 createGrid();
 hover();
